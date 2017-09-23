751539c888f161416f43c54da41423c215b424d1
kobe <kobe@vps-MacBook-Air.local>
2017-09-22 21:36:29 -0700

    updating routes, api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
+  // Gets all articles
   getArticles: function() {
     return axios.get("/api/articles");
   },
-  // Gets the book with the given id
+  // Gets the article with the given id
   getArticle: function(id) {
     return axios.get("/api/articles/" + id);
   },
-  // Deletes the book with the given id
+  // Deletes the article with the given id
   deleteArticle: function(id) {
     return axios.delete("/api/articles/" + id);
   },
-  // Saves a book to the database
+  // Saves an article to the database
   saveArticle: function(articleData) {
     return axios.post("/api/articles", articleData);
   }
